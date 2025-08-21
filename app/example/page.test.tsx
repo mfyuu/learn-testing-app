a1f2ec335e06902c425526e706cab67061e936be
mfyuu <83203852+ve1997@users.noreply.github.com>
2025-08-21 19:10:04 +0900

    Revert "debug"
    This reverts commit fff50e9af7ed3793ef81dc25e5a3181552db99b6.

--- app/example/page.test.tsx
+++ app/example/page.test.tsx
@@ -4,5 +4,5 @@ import Page from "./page";
 
 test("Page", () => {
 	render(<Page />);
-	expect(screen.getByRole("heading", { level: 2, name: "Home" })).toBeDefined();
+	expect(screen.getByRole("heading", { level: 1, name: "Home" })).toBeDefined();
 });
